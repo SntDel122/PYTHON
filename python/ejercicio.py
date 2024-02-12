9e5a874f3a09c17d40bb429bf45588315417003b
SntDel122 <159148356+SntDel122@users.noreply.github.com>
2024-02-12 14:46:53 -0500

    Add files via upload

---
+++ python/ejercicio.py
@@ -0,0 +1,10 @@
+nota1=float(3.6)
+nota2=float(2.9)
+nota3=float(3.3)
+
+peso_nota1=0.2
+peso_nota2=0.35
+peso_nota3=0.45
+
+suma_total=(nota1*peso_nota1)+(nota2*peso_nota2)+(nota3*peso_nota3)
+print(suma_total)
