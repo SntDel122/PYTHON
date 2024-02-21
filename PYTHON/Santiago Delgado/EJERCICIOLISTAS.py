8d9d73e0013498cf87ea32732e52619f54431b31
SntDel122 <159148356+SntDel122@users.noreply.github.com>
2024-02-21 09:31:38 -0500

    Add files via upload

---
+++ PYTHON/Santiago Delgado/EJERCICIOLISTAS.py
@@ -0,0 +1,21 @@
+#Ejercicio listas
+valor_a = 5
+#              0 1 2 3 4  5  6  7  8
+valor_lista = [4,7,8,9,10,20,30,40,55]
+print(valor_lista [8])
+
+#ejercicio lista texto
+lista_2 = ["saludo","juan","estudiante","incap"]
+print(lista_2)
+
+#ejercicio listas aplicando apppend
+lista_2.append('prueba')
+print(lista_2)
+
+#ejercicio listas aplicando pop
+lista_2.pop(1)
+print(lista_2)
+
+#ejercicio lista aplicando clear
+lista_2.clear()
+print(lista_2)
